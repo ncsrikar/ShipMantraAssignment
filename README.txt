eb9ce73475e2a32c17bd5fa77f42d9f2000104d0
Srikar <ncsrikar@gmail.com>
2022-08-20 16:02:50 +0530

    Initial repo

---
+++ README.txt
@@ -0,0 +1,17 @@
+The folder consists of both FRONTEND Written in Angular and Backend Written in NodeJS. 
+
+Although I was supposed to create a simple webpage to get data from Borzo. I have decided to use Angular as frontend for it. 
+It is possible to achive the same using 
+
+NodeJS backend is communicating on Port 3000. Angular is communicating on the Port 4200. 
+
+To run Backend => 
+cd shipmantra_backend
+node shipmantra.js
+
+
+to run FrontEnd =>
+cd shipmantra_frontend
+ng serve -o
+
+
---
+++ shipmantra_backend/shipMantra.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const request = require('request'); 
+const cors = require('cors');
+const app = express()
+const port = 3000
+app.use(cors({
+  origin:"*",
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}))
+var JsonParser = express.json() //Can also be written as app.use(bodyParser.json()) to avoid explicitly stating.
+
+//Options for calling the BORZO API FROM this Backend.
+const options = {
+  url: 'https://robotapitest-in.borzodelivery.com/api/business/1.2/calculate-order',
+  method: 'POST',
+  headers: {
+      'Accept': 'application/json',
+      'Accept-Charset': 'utf-8',
+      'X-DV-Auth-Token':'25F41401FA4233096E0AD8A57888123456AE101F',
+  },
+  
+};
+
+app.post('/', JsonParser,(req, res) => {  
+  options.json = {"matter":"Documents","points":[{"address":req.body.from_address},{"address":req.body.to_address}]}
+  console.log(options)
+  request(options, function(err,res1, body){
+    res.send({"order_amount":body.order.payment_amount, "from_address":req.body.from_address,"to_address":req.body.to_address})
+  })  
+})
+app.listen(port, () => {
+  console.log(`ShipMantra example app listening on port ${port}`)
+})
