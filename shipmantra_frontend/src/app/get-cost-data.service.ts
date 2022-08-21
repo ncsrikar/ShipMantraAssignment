c27595d8dea4c0ab43ab6cdd201e763250a8211e
Srikar <ncsrikar@gmail.com>
2022-08-21 17:11:59 +0530

    changed backend url

--- shipmantra_frontend/src/app/get-cost-data.service.ts
+++ shipmantra_frontend/src/app/get-cost-data.service.ts
@@ -12,7 +12,7 @@ export class GetCostDataService {
 
   getCostData(from_address:string, to_address:string){
     let data = {"from_address":from_address, "to_address":to_address}
-    this.http.post("http://localhost:3000",data).subscribe(res => {
+    this.http.post("https://shipmantra-assignment.herokuapp.com",data).subscribe(res => {
     let res_parsed = JSON.parse(JSON.stringify(res))
     console.log(res_parsed["order_amount"])
     this.router.navigate(["/showCost"], {
