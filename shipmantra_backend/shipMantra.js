11496f46a2932a99e6ee3f402047cd3eab45003b
Srikar <ncsrikar@gmail.com>
2022-08-21 14:53:09 +0530

    Adding heruko support

--- shipmantra_backend/shipMantra.js
+++ shipmantra_backend/shipMantra.js
@@ -2,7 +2,7 @@ const express = require('express')
 const request = require('request'); 
 const cors = require('cors');
 const app = express()
-const port = 3000
+var port = process.env.PORT || 3000
 app.use(cors({
   origin:"*",
   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
